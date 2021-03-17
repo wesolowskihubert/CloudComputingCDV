8ce2ceb5541b00dc632b9380b64f0588991d0977
Hubert <hwesolowski@edu.cdv.pl>
2021-03-17 20:07:32 +0100

    Dodaj pliki projektów.

---
+++ PeopleWebApp/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PeopleWebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var people = new List<Person>
+            {
+                new Person{
+                    FirstName = "Hubert",
+                    LastName = "Wesołowski",
+                    Id = 1
+                }
+            };
+            return Ok(people);
+        }
+    }
+}
---
+++ PeopleWebApp/PeopleDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWebApp
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions<PeopleDb> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
